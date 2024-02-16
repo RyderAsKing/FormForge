d8840d7a5e9480faa4f538ab1bc19ed365cb62c7
Rajat Asthana <clasherlodt@gmail.com>
2024-02-16 20:49:14 +0530

    added forms model and migration

---
+++ app/Models/Form.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\SchemalessAttributes\Casts\SchemalessAttributes;
+
+class Form extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'status', 'fields'];
+
+    protected $casts = [
+        'fields' => SchemalessAttributes::class,
+    ];
+
+    public function scopeWithFields(): Builder
+    {
+        return $this->fields->modelScope();
+    }
+}
