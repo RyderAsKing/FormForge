1caa0e28c3f549af15d3a088d0c6046f05efe8da
Rajat Asthana <clasherlodt@gmail.com>
2024-02-17 03:29:26 +0530

    changed the application logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<h1 class="text-2xl px-2 py-2 bg-gray-800 text-white rounded-xl">{{config('app.name', 'FormForge')}}</h1>
+<h1 class="text-xl px-4 py-2 bg-gray-800 text-white rounded-xl">{{config('app.name', 'FormForge')}}</h1>
